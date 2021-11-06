theme-controller/thcon

commit 67f2fda52c2b7a5437c2ba34076fc32bf0902711

vscode: allow settings.json to be missing completely
On a fresh install (or a system without Visual Studio Code used at all),
there's likely to be no settings.json file on-disk.  Consider a missing
settings.json as a success, since the user likely intends to not have
that file.

fixes #1

thcon/src/app/vscode.rs
@@ -44,10 +44,11 @@
 //! | `light` | string | The theme to use in light mode | Default Light+ |
 //! | `config` | string | Absolute path to your `settings.json` file | `~/.config/Code/User/settings.json` |
 
-use std::fs;
+use std::{fs, io};
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
+use anyhow::anyhow;
 use log::{debug, error};
 use regex::{Captures, Regex};
 use serde::Deserialize;
@@ -119,7 +120,6 @@ impl Themeable for VSCode {
             &settings_path.display()
         );
         match fs::read_to_string(self.settings_json_path())
-            .with_context(|| format!("Unable to read settings from {}", &settings_path.display()))
         {
             Ok(settings) => {
                 let settings = replace_color_theme(&settings, theme);
@@ -128,8 +128,16 @@ impl Themeable for VSCode {
                 })
             }
             Err(e) => {
-                error!("Unable to read settings: {}", e);
-                Err(e)
+                if e.kind() == io::ErrorKind::NotFound {
+                    // allow settings.json to not exist at all
+                    Ok(())
+                } else {
+                    Err(
+                        anyhow::Error::new(e).context(format!(
+                            "Unable to read settings from {}", &settings_path.display()
+                        ))
+                    )
+                }
             }
         }
     }

commit 98915874709a619234cba09c3db50794eca5c8f2

vscode: fix lint failures introduced in 67f2fda (#3)

thcon/src/app/vscode.rs
@@ -44,12 +44,11 @@
 //! | `light` | string | The theme to use in light mode | Default Light+ |
 //! | `config` | string | Absolute path to your `settings.json` file | `~/.config/Code/User/settings.json` |
 
-use std::{fs, io};
 use std::path::PathBuf;
+use std::{fs, io};
 
 use anyhow::{Context, Result};
-use anyhow::anyhow;
-use log::{debug, error};
+use log::debug;
 use regex::{Captures, Regex};
 use serde::Deserialize;
 
@@ -119,8 +118,7 @@ impl Themeable for VSCode {
             "Reading/writing settings.json at {}",
             &settings_path.display()
         );
-        match fs::read_to_string(self.settings_json_path())
-        {
+        match fs::read_to_string(self.settings_json_path()) {
             Ok(settings) => {
                 let settings = replace_color_theme(&settings, theme);
                 fs::write(self.settings_json_path(), settings).with_context(|| {
@@ -132,11 +130,10 @@ impl Themeable for VSCode {
                     // allow settings.json to not exist at all
                     Ok(())
                 } else {
-                    Err(
-                        anyhow::Error::new(e).context(format!(
-                            "Unable to read settings from {}", &settings_path.display()
-                        ))
-                    )
+                    Err(anyhow::Error::new(e).context(format!(
+                        "Unable to read settings from {}",
+                        &settings_path.display()
+                    )))
                 }
             }
         }