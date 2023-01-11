1503777c0920a1c2614fa9c8eda463ee2eed1fda
Sean Barag <sean@barag.org>
2023-01-11 15:47:41 -0600

    gtk3: use health for ValidateConfig

--- lib/apps/gtk3.go
+++ lib/apps/gtk3.go
@@ -5,8 +5,8 @@ package apps
 import (
 	"context"
 
-	goValidator "github.com/go-playground/validator/v10"
 	"github.com/gotk3/gotk3/glib"
+	"github.com/theme-controller/thcon/lib/health"
 	"github.com/theme-controller/thcon/lib/operation"
 )
 
@@ -24,12 +24,8 @@ func NewGtk3() Switchable {
 	return &Gtk3{}
 }
 
-func (g *Gtk3) ValidateConfig(ctx context.Context, validator *goValidator.Validate, config *Config) error {
-	if config.Gtk3 == nil {
-		return nil
-	}
-
-	return validator.StructCtx(ctx, config.Gtk3)
+func (g *Gtk3) ValidateConfig(ctx context.Context, config *Config) (health.Status, error) {
+	return health.HasDefaults(ctx, config.Gtk3)
 }
 
 func (g *Gtk3) Switch(ctx context.Context, mode operation.Operation, config *Config) error {
