706c9e0b3bcda61e7379f797067d1a9d5a52a0e8
Sean Barag <sean@barag.org>
2023-06-10 14:58:43 -0700

    ci: set up Cirrus CI for build, lint, test jobs
    GitHub Actions currently handles this, but doesn't support FreeBSD. Set
    up Cirrus CI for build, lint, and test tasks on linux, freebsd, and
    darwin, instead of GitHub Actions.

---
+++ support/maybe-upload-artifact.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Originally from https://cirrus-ci.org/examples/#release-assets
+
+set -eo pipefail
+
+if [[ "$CIRRUS_RELEASE" == "" ]]; then
+  echo "Not a release. No need to deploy!"
+  exit 0
+fi
+
+if [[ "$GITHUB_TOKEN" == "" ]]; then
+  echo "Please provide GitHub access token via GITHUB_TOKEN environment variable!"
+  exit 1
+fi
+
+if [[ "$1" == "" ]]; then
+  echo "Please provide a destination filename (shown on the Github Releases page) as \$1."
+  exit 1
+fi
+
+file_content_type="application/octet-stream"
+fpath="./thcon"
+name=$1
+url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
+
+echo "Uploading $fpath..."
+curl -X POST \
+  --data-binary @$fpath \
+  --header "Authorization: token $GITHUB_TOKEN" \
+  --header "Content-Type: $file_content_type" \
+  $url_to_upload
+
+
